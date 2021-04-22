54d00ce5bdcdaa8e6c20ffd7aedb434288731a9c
almoraboy <68150888+almoraboy@users.noreply.github.com>
2021-04-22 15:38:15 +0530

    GremlinParser now parses nested functions.

--- gremlinparser.py
+++ gremlinparser.py
@@ -6,25 +6,20 @@ class GremlinSyntaxError( Exception ):
 	pass
 
 class GremlinToken:
-	GREMLIN_IDENTIFIER, GREMLIN_ASSIGN, GREMLIN_FUNCTION = 'IDENTIFIER', 'ASSIGN', 'FUNCTION'
+	GREMLIN_IDENTIFIER, GREMLIN_ASSIGN, GREMLIN_FUNCTION, GREMLIN_LITERAL = 'IDENTIFIER', 'ASSIGN', 'FUNCTION', 'LITERAL'
 	
 	def __init__( self, tokenType ):
-		assert tokenType in (GremlinToken.GREMLIN_IDENTIFIER, GremlinToken.GREMLIN_ASSIGN, GremlinToken.GREMLIN_FUNCTION)
-		self._tokenType = tokenType
-
-	def tokenType( self ):
-		return self._tokenType
+		assert tokenType in (GremlinToken.GREMLIN_IDENTIFIER, GremlinToken.GREMLIN_ASSIGN, GremlinToken.GREMLIN_FUNCTION,
+			                 GremlinToken.GREMLIN_LITERAL)
+		self.tokenType = tokenType
 
 class GremlinIdentifier( GremlinToken ):
 	def __init__( self, identifierName ):
-		self._identifierName = identifierName
+		self.identifierName = identifierName
 		GremlinToken.__init__( self, GremlinToken.GREMLIN_IDENTIFIER )
 
-	def identifierName( self ):
-		return self._identifierName
-
 	def __repr__( self ):
-		return 'GremlinIdentifier @{} identifierName={}'.format( id( self ), self._identifierName )
+		return 'GremlinIdentifier @{} identifierName={}'.format( id( self ), self.identifierName )
 
 class GremlinAssign( GremlinToken ):
 	def __init__( self ):
@@ -37,18 +32,21 @@ class GremlinFunction( GremlinToken ):
 	def __init__( self, functionName, argumentList=None ):
 		self.functionName = functionName
 		self.argumentList = argumentList or list()
-		self.calls = None
 		GremlinToken.__init__( self, GremlinToken.GREMLIN_FUNCTION )
 
 	def addArgument( self, argument ):
 		self.argumentList.append( argument )
 
-	def setCalls( self, calledFunction ):
-		assert isinstance( calledFunction, GremlinFunction )
-		self.calls = calledFunction
+	def __repr__( self ):
+		return 'GremlinFunction @{} functionName={} argumentList={}'.format( id( self ), self.functionName, self.argumentList )
+
+class GremlinLiteral( GremlinToken ):
+	def __init__( self, literal ):
+		self.literal = literal
+		GremlinToken.__init__( self, GremlinToken.GREMLIN_LITERAL )
 
 	def __repr__( self ):
-		return 'GremlinFunction @{} functionName={} argumentList={}'.format( id( self ), self.functionName, self.argumentList ) 
+		return 'GremlinLiteral @{} literal={}'.format( id( self ), self.literal )
 
 class GremlinParser:
 	@staticmethod
@@ -61,16 +59,21 @@ class GremlinParser:
 		return gremlinTokenList
 
 	@staticmethod
-	def _processFunction( tokenList, gremlinTokenList ):
+	def _processFunction( tokenList, gremlinFunction ):
 		while True:
 			tokenType, tokenValue, * _ = next( tokenList )
 			if tokenType == tokenize.OP and tokenValue == ')':
 				break
 			if tokenType == tokenize.STRING:
-				argument = tokenValue[ 1 : -1 ] # tokenValue includes quotes and is of the form: 'Apple'. Remove quotes before adding to the argument list.
-				gremlinTokenList[ -1 ].addArgument( argument )
+				literal = tokenValue[ 1 : -1 ] # tokenValue includes quotes and is of the form: 'Apple'. Remove quotes before adding to the argument list.
+				gremlinFunction.addArgument( GremlinLiteral( literal ) )
 			elif tokenType == tokenize.NUMBER:
-				gremlinTokenList[ -1 ].addArgument( tokenValue )
+				literal = tokenValue
+				gremlinFunction.addArgument( GremlinLiteral( literal ) )
+			elif tokenType == tokenize.NAME:
+				nestedGremlinFunction = GremlinFunction( functionName=tokenValue )
+				gremlinFunction.addArgument( nestedGremlinFunction )
+				GremlinParser._processFunction( tokenList, nestedGremlinFunction )
 
 	@staticmethod
 	def _process( tokenList, gremlinTokenList ):
@@ -83,8 +86,9 @@ class GremlinParser:
 		
 		nextTokenType, nextTokenValue, * _ = next( tokenList )
 		if nextTokenType == tokenize.OP and nextTokenValue == '(':
-			gremlinTokenList.append( GremlinFunction( functionName=tokenValue ) )
-			GremlinParser._processFunction( tokenList, gremlinTokenList )
+			gremlinFunction = GremlinFunction( functionName=tokenValue )
+			gremlinTokenList.append( gremlinFunction )
+			GremlinParser._processFunction( tokenList, gremlinFunction )
 		elif nextTokenType == tokenize.OP and nextTokenValue in ('.', '='):
 			gremlinTokenList.append( GremlinIdentifier( identifierName=tokenValue ) )
 			if nextTokenValue == '=':
@@ -95,9 +99,9 @@ class GremlinParser:
 
 class GremlinParserTest( unittest.TestCase ):
 	def test_GremlinParser( self ):
-		commandString = "i = g.V().has( 'name', 'New York' ).out( 'connects' ).has( 'name', 'Boulder' ).values( 'companies' )"
-		print( commandString )
-		for gremlinToken in GremlinParser.parse( commandString ):
+		gremlinQuery = "i = g.V().has( 'name', 'New York' ).out( 'connects' ).map( values( 'name' ) )"
+		print( gremlinQuery )
+		for gremlinToken in GremlinParser.parse( gremlinQuery ):
 			print( gremlinToken )
 
 if __name__ == '__main__':
